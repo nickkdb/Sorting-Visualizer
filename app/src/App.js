39dc67ce5d154929aa8200acdb51a86a9152f255
wutango <nickkdb@gmail.com>
2021-05-03 12:02:11 -0400

    uploading React app and file structure

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Application from "./pages/Application";
+
+function App() {
+  return (
+    <div className="App">
+      <Application />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/pages/Application.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Application() {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default Application;
