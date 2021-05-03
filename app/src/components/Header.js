a6f9993a17a5c35a430120cd003b502077db1a91
wutango <nickkdb@gmail.com>
2021-05-03 12:05:08 -0400

    uploading page header

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,12 +1,42 @@
 import React from "react";
+import {Nav, Navbar, Row, Col} from 'react-bootstrap';
 
 function Header() {
 
+    const style= {
+        fontFamily: "Courier New",
+        fontSize: "1.25rem",
+        color: "#B0E0E6",
+      }
+    const algoStyle= {
+      fontFamily: "Courier New",
+      fontSize: "1rem",
+      color: "#B0E0E6",
+    }
 
     return (
-        <>
-        </>
-    )
+        <Navbar className="py-4" style={{backgroundColor: "steelblue"}} collapseOnSelect expand="lg" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav style={{width: "100%"}}>
+              <Row style={{width: "100%", textAlign: 'center'}}>
+                  <Col xl={4}>
+            <Nav.Link style={style}>Generate New Array</Nav.Link>
+                  </Col>
+                  <Col xl={4}>
+            <Nav.Link style={style}>Sorting Speed</Nav.Link>
+                  </Col>
+                  <Col xl={4} style={{display: "grid", gridTemplateColumns: "repeat(3, 1fr)"}}>
+            <Nav.Link style={algoStyle}>Merge Sort</Nav.Link>
+            <Nav.Link style={algoStyle}>Bubble Sort</Nav.Link>
+            <Nav.Link style={algoStyle}>Selection Sort</Nav.Link>
+                  </Col>
+              </Row>
+          </Nav>
+          
+        </Navbar.Collapse>
+      </Navbar>
+      )
 }
 
 export default Header;
