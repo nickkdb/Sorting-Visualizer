6cb07e33b1cd2f13e6f755b633fed30ef21643c0
wutango <nickkdb@gmail.com>
2021-05-03 14:32:30 -0400

    selection sort renders frames

--- app/src/components/selection.js
+++ app/src/components/selection.js
@@ -1,5 +1,27 @@
 import {setWidth } from "../utils/width";
 
-export const startSelection= (arr) => {
-console.log(arr);
+const SPEED= 5;
+let array;
+let divs;
+let lastRender;
+let smallest;
+let indexOfSmallest;
+let startingIndex;
+
+
+export const startSelection= (input) => {
+lastRender= 0;
+divs= document.getElementById('container').querySelectorAll('array');
+array= input;
+smallest= null;
+startingIndex= 0;
+requestAnimationFrame(runLoop);
+}
+
+const runLoop= (timeStamp) => {
+    requestAnimationFrame(runLoop);
+    let timeTilRender= (timeStamp - lastRender) / 1000;
+    if (timeTilRender < 1 / SPEED) return;
+    lastRender= timeStamp;
+    console.log('rendering!');
 }
